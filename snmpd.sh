14f43cbd457cf42f16e2241f528435483bbedf5f
Nicholas Marus <nmarus@gmail.com>
2016-03-13 00:48:13 -0500

    Updated

--- snmpd.sh
+++ snmpd.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-[ -r /etc/default/snmpd ] && . /etc/default/snmpd
-exec /usr/sbin/snmpd >> /var/log/snmpd.log 2>&1
+OLD_MIBS_DIR="/usr/share/mibs/site:/usr/share/snmp/mibs:/usr/share/mibs/iana:/usr/share/mibs/ietf:/usr/share/mibs/netsnmp"
+MIBS_DIR="/usr/share/snmp/mibs:/usr/share/snmp/mibs/iana:/usr/share/snmp/mibs/ietf"
+export MIBDIRS="$MIBS_DIR:$OLD_MIBS_DIR"
+SNMPDRUN="yes"
+SNMPDOPTS='-Lsd -Lf /dev/null -f -u snmp -g snmp -I -smux,mteTrigger,mteTriggerConf -p /var/run/snmpd.pid'
+exec /usr/sbin/snmpd $SNMPDOPTS
